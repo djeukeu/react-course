d678359d24667569bef4ace1a0d36b4c347cd851
djeukeu <dmxchristian@gmail.com>
2022-07-11 12:23:03 +0100

    end

--- end/src/App.jsx
+++ end/src/App.jsx
@@ -1,37 +1,22 @@
-import React from 'react';
-import Expenses from './components/Expenses/Expenses';
-import './app.css'
+import React, { useContext } from 'react';
 
-const App = () => {
-	
-	const expenses = [
-		{
-		  id: 'e1',
-		  title: 'Toilet Paper',
-		  amount: 94.12,
-		  date: new Date(2020, 7, 14),
-		},
-		{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-		{
-		  id: 'e3',
-		  title: 'Car Insurance',
-		  amount: 294.67,
-		  date: new Date(2021, 2, 28),
-		},
-		{
-		  id: 'e4',
-		  title: 'New Desk (Wooden)',
-		  amount: 450,
-		  date: new Date(2021, 5, 12),
-		},
-	  ];
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
-	  return (
-		<div>
-		  <h2 className="title">Let's get started!</h2>
-		  <Expenses items={expenses} />
-		</div>
-	  );
-};
+function App() {
+  const ctx = useContext(AuthContext);
+
+  return (
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
+  );
+}
 
 export default App;
